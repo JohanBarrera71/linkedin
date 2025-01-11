1b86067016b6ddb4539f38fb6e0712f49d0bcbe4
JohanBarrera71 <johan.dev.71@gmail.com>
2025-01-11 15:19:54 -0600

    feat: implement frontend authentication

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,44 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.scss";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Feed } from "./features/feed/pages/Feed";
+import { Login } from "./features/auth/pages/Login/Login";
+import { Signup } from "./features/auth/pages/Signup/Signup";
+import { VerifyEmail } from "./features/auth/pages/VerifyEmail/VerifyEmail";
+import { ResetPassword } from "./features/auth/pages/ResetPassword/ResetPassword";
+import { AuthContextProvider } from "./features/auth/context/AuthContextProvider";
 
-createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    element: <AuthContextProvider />,
+    children: [
+      {
+        path: "/",
+        element: <Feed />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/signup",
+        element: <Signup />,
+      },
+      {
+        path: "/request-password-reset",
+        element: <ResetPassword />,
+      },
+      {
+        path: "/verify-email",
+        element: <VerifyEmail />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
